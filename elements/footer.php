bb05a40019096724aa2d8ed584e26baf8df9d3d7
AngelaRg <angela_9412@hotmail.com>
2016-03-11 08:23:18 -0600

    Consistencia y css de hr

--- elements/footer.php
+++ elements/footer.php
@@ -1,7 +1,7 @@
 <footer>
     <nav class="navbar navbar-default">
         <div class="container">
-            <p class="navbar-text">Hydrosumintros Inteligentes S.A. de C.V.</p>
+            <p class="navbar-text">Hydrosuministros Inteligentes S.A. de C.V.</p>
             <p class="navbar-text">Número de visitas:</p>
             <p class="navbar-text navbar-right">Tel: (81) 1234 2678&nbsp;&nbsp;&nbsp;&nbsp;Correo: ventas@hydrosumint.mx</p>
         </div>
