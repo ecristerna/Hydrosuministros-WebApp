8b731d992421348a6f004121f6e38a64788fae8b
Cesar <cesars90_@hotmail.com>
2016-04-15 08:23:04 -0500

    Primera implementacion del formulario de reparaciones con imagen.

--- formulario_reparaciones.php
+++ formulario_reparaciones.php
@@ -23,20 +23,20 @@ include_once('elements/header.php');
             <p>
                 Lorem ipsum dolor sit amet, consectetur adipiscing elit. Mauris porttitor orci dui, a eleifend metus consectetur nec. Nullam purus elit, malesuada sed sagittis sit amet, mollis sed justo. Morbi ante lectus, pretium vel accumsan ut, lobortis non mi. Donec faucibus euismod enim at rhoncus. In vehicula sodales egestas. Aenean lobortis, erat eu condimentum iaculis, nunc odio molestie turpis, ut tristique mi arcu et metus. Morbi facilisis risus ut euismod finibus.
             </p>
-            <form>
+            <form action="/services/repairService.php" method="post" enctype="multipart/form-data">
                 <div class="form-group">
                     <div class="row">
                         <div class="col-sm-6 form-group">
                             <input class="form-control" id="Name" name="Nombre" placeholder="Nombre" type="text" required>
                         </div>
                         <div class="col-sm-6 form-group">
-                            <input class="form-control" id="Email" name="Empresa" placeholder="Empresa" type="text" required>
+                            <input class="form-control" id="Empresa" name="Empresa" placeholder="Empresa" type="text" required>
                         </div>
                     </div>
 
                     <div class="row">
                         <div class="col-sm-6 form-group">
-                            <input class="form-control" id="Nombre" name="Telefono" placeholder="Télefono" type="text" required>
+                            <input class="form-control" id="Telefono" name="Telefono" placeholder="Télefono" type="text" required>
                         </div>
                         <div class="col-sm-6 form-group">
                             <input class="form-control" id="Email" name="Email" placeholder="Email" type="email" required>
@@ -44,7 +44,7 @@ include_once('elements/header.php');
                     </div>
 
                     <label class="control-label">Imagen de la pieza:</label>
-                    <input id="input-1" type="file" class="file">
+                    <input id="Imagen" type="file" class="file">
                     <br/>
                     <label for="comment">Descripción:</label>
                     <textarea class="form-control" rows="5" id="comment"></textarea>
