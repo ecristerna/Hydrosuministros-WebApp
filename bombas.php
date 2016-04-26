d2f30302f726fc1f935a7736bbdc2f662617c1fd
AngelaRg <angela_9412@hotmail.com>
2016-04-26 08:31:43 -0500

    Arreglo de imagenes situadas.

--- bombas.php
+++ bombas.php
@@ -19,11 +19,11 @@ include_once('elements/header.php');
             <hr>
 
             <div class="row productsrow">
-                <div class="col-md-2">
-                    <img class="img img-responsive siteimages" src="images/bomba1.png" >
+                <div class="col-md-3">
+                    <img class="img img-responsive siteimages" src="images/pistonesArreglada.png" >
                 </div>
 
-                <div class="col-md-10">
+                <div class="col-md-9">
                     <h4>Bombas y Motores de Pistones</h4>
                     <p class="text">
                         Si usted está en busca de una unidad nueva o remanufacturada, nuestros conocimientos técnicos así como nuestro vasto inventario nos convierte en su ventanilla única de servicio, ya que manejamos bombas de pistones Nuevas Originales, Nuevas Genéricas  y Remanufacturadas de las marcas líders en la Industria tales como: Vickers®, Eaton®, Kawasaki®, Rexroth®, Sundstrand®, Linde®, Denison®, Racine®, Parker® y Oilgear®, por nombrar algunos. Nuestros componentes nuevos, genéricos o remanufacturadas son fabricados con partes que cumplen o exceden la especificación OEM, en el caso de reman, estas son totalmente probadas en nuestros bancos antes de que dejen nuestras instalaciones. Nuestro inventario y capacidades nos permiten soportar un amplio rango de aplicaciones industriales que van desde pequeño equipos hasta equipos industriales a gran escala.
@@ -31,14 +31,14 @@ include_once('elements/header.php');
                 </div>
             </div>
             <div class="row productsrow">
-                <div class="col-md-10">
+                <div class="col-md-9">
                     <h4>Bombas y Motores de Paletas</h4>
                     <p class="text">
                         Hydrosuministros ofrece una gran selección de bombas y motores de paletas tanto en componentes nuevos, genéricos y unidades remanufacturadas. Esto incluye bombas sencillas, dobles y triples con una amplia variedad de desplazamientos, bridas de montaje y flechas, además de mencionar que algunos modelos están disponibles con capacidades "Thru-drive". Ofrecemos componentes de los siguientes Fabricantes: Vickers® y Denison®. Usted puede confiar en nosotros para reparar cualquier componente hidráulico industrial incluyendo clásicos y por supuesto obsoletos.
                     </p>
                 </div>
-                <div class="col-md-2">
-                    <img class="img img-responsive siteimages" src="images/bomba2.png" >
+                <div class="col-md-3">
+                    <img class="img img-responsive siteimages" src="images/paletasArreglada.png" >
                 </div>
             </div>
         </div>
--- motores.php
+++ motores.php
@@ -19,11 +19,11 @@ include_once('elements/header.php');
             <hr>
 
             <div class="row productsrow">
-                <div class="col-md-2">
+                <div class="col-md-3">
                     <img class="img img-responsive siteimages" src="images/motor1.png" >
                 </div>
 
-                <div class="col-md-10">
+                <div class="col-md-9">
                     <h4>Motores Orbitales</h4>
                     <p class="text">
                         Nuestra marca de Motores Orbitales es su mejor opción para aplicaciones de baja velocidad y torque alto con  un diseño compacto y económico. Usando la tecnología probada de geroler, estos motores cuentan con alta eficiencia, operación suave y una larga vida de operativa. Los motores vienen equipados con un sello de alta presión en la flecha. Contamos con un amplio rango de desplazamientos, opciones de brida de montaje y flechas. Son un remplazo funcional para motores comunes en el mercado actual tales como: Eaton®/Char-Lynn®, Danfoss®, Prker® y White®.
