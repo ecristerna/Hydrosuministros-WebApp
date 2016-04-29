6b3c917c9fa88109c392482cad2f91000ceffccb
Eduardo Cristerna <eduardo@schoolhub.com>
2016-04-29 07:55:52 -0500

    mail

--- services/repairService.php
+++ services/repairService.php
@@ -14,16 +14,34 @@ $nombre = $_POST["Nombre"];
 $empresa = $_POST["Empresa"];
 $telefono = $_POST["Telefono"];
 $email = $_POST["Email"];
-$descripcion = $_POST["comment"];
+$descripcion = htmlspecialchars($_POST["comment"]);
+
+$mail = new PHPMailer;
+
+$mail->SMTPDebug = 3;                               // Enable verbose debug output
+
+$mail->IsSMTP(); //send via SMTP
+// info@hydrosumint.mx
+
+$mail->Host = "localhost";
+
+$mail->setFrom($email, $nombre);
+$mail->addAddress("ecristerna@icloud.com");     // Add a recipient
+
+$mail->isHTML(true);                                  // Set email format to HTML
+
+$mail->Subject = "Solicitud de Reparación";
 
 // image upload handler
 $target_dir = "../images/";
+echo basename($_FILES["Imagen"]["name"]);
 $target_file = $target_dir . basename($_FILES["Imagen"]["name"]);
 $uploadOk = 1;
-$imageFileType = pathinfo($target_file,PATHINFO_EXTENSION);
+$imageFileType = pathinfo($target_file, PATHINFO_EXTENSION);
 // Check if image file is a actual image or fake image
 if(isset($_POST["submit"])) {
     $check = getimagesize($_FILES["Imagen"]["tmp_name"]);
+
     if($check !== false) {
         $uploadOk = 1;
     } else {
@@ -33,43 +51,31 @@ if(isset($_POST["submit"])) {
 }
 
 // Allow certain file formats
-if($imageFileType != "jpg" && $imageFileType != "png" && $imageFileType != "jpeg"
-    && $imageFileType != "gif" ) {
+if($imageFileType != "jpg" && $imageFileType != "png" && $imageFileType != "jpeg" && $imageFileType != "gif" ) {
     $uploadOk = 0;
 }
-// Check if $uploadOk is set to 0 by an error
-if ($uploadOk == 0) {
-    echo "Sorry, your file was not uploaded.";
-// if everything is ok, try to upload file
-} else {
+
+if ($uploadOk == 1) {
     if (move_uploaded_file($_FILES["Imagen"]["tmp_name"], $target_file)) {
         echo "The file ". basename( $_FILES["Imagen"]["name"]). " has been uploaded.";
     } else {
         echo "Sorry, there was an error uploading your file.";
     }
+
+    $mail->AddEmbeddedImage($target_file, "my-attach", $target_file);
+
+    $mail->Body    = "Nombre: " . $nombre . "<br>" . "Empresa: " . $empresa . "<br>" .  "Telefono: " . $telefono . "<br>" .
+        "Correo: " . $email . "<br>" . "Comentarios: " . $descripcion . "<br><br><br>" .
+        '<img alt="PHPMailer" src="cid:my-attach">';
+    $mail->AltBody = "Nombre: " . $nombre . "\n" . "Empresa: " . $empresa . "\n" . "Telefono: " . $telefono.  "\n" .
+        "Correo: " . $email  . "\n" . "Comentarios: " . $descripcion . "\n\n\n" .
+        '<img alt="PHPMailer" src="cid:my-attach">';
+} else {
+    $mail->Body    = "Nombre: " . $nombre . "<br>" . "Empresa: " . $empresa . "<br>" .  "Telefono: " . $telefono . "<br>" .
+        "Correo: " . $email . "<br>" . "Comentarios: " . $descripcion . "<br><br><br>";
+    $mail->AltBody = "Nombre: " . $nombre . "\n" . "Empresa: " . $empresa . "\n" . "Telefono: " . $telefono.  "\n" .
+        "Correo: " . $email . "\n" . "Comentarios: " . $descripcion . "\n\n\n";
 }
-
-$mail = new PHPMailer;
-
-$mail->SMTPDebug = 3;                               // Enable verbose debug output
-
-$mail->IsSMTP(); //send via SMTP
-
-$mail->Host = "localhost";
-
-$mail->setFrom($email, $nombre);
-$mail->addAddress("ecristerna@icloud.com");     // Add a recipient
-
-$mail->isHTML(true);                                  // Set email format to HTML
-
-$mail->AddEmbeddedImage($target_file, "my-attach", $target_file);
-$mail->Subject = "Solicitud de reparacion";
-$mail->Body    = "Nombre: " . $nombre . "\n" . "Empresa: " . $empresa . "\n" .  "Telefono: " . $telefono . "\n" .
-    "Correo: " . $email . "\n" .
-    'Imagen: <img alt="PHPMailer" src="cid:my-attach">';
-$mail->AltBody = "Nombre: " . $nombre . "\n" . "Empresa: " . $empresa . "\n" . "Telefono: " . $telefono.  "\n" .
-    "Correo: " . $email . "\n" .
-    'Imagen: <img alt="PHPMailer" src="cid:my-attach">';;
 
 if(!$mail->send()) {
     header('HTTP/1.1 406 User not found');
@@ -78,6 +84,6 @@ if(!$mail->send()) {
     echo json_encode("Message has been sent");
 }
 
-header("Location: reparaciones.php");
+header("Location: ../formulario_reparaciones.php");
 die();
 ?>
