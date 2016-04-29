8133865481b081cdbad210e6e45bb408c6526fa9
AngelaRg <angela_9412@hotmail.com>
2016-04-29 08:16:48 -0500

    Texto en inventario real y con CSS correcto.

--- inventario.php
+++ inventario.php
@@ -20,10 +20,11 @@ if(!isset($_SESSION["username"])) {
     <div class="container-fluid">
 
         <div class="col-md-12">
-            <h1>Inventario</h1>
+            <h2>Inventario</h2>
             <hr>
-            <p>
-                Lorem ipsum dolor sit amet, consectetur adipiscing elit. Mauris porttitor orci dui, a eleifend metus consectetur nec. Nullam purus elit, malesuada sed sagittis sit amet, mollis sed justo. Morbi ante lectus, pretium vel accumsan ut, lobortis non mi. Donec faucibus euismod enim at rhoncus. In vehicula sodales egestas. Aenean lobortis, erat eu condimentum iaculis, nunc odio molestie turpis, ut tristique mi arcu et metus. Morbi facilisis risus ut euismod finibus. Fusce facilisis felis in orci dictum, id malesuada nulla facilisis. Etiam dolor purus, ultrices ac rutrum nec, condimentum blandit magna. Integer non convallis nibh. Maecenas molestie ligula non dui efficitur, nec lobortis orci faucibus. Nullam maximus nulla quam. Pellentesque a facilisis sapien. Nam at massa diam.
+            <p class="text">
+                Tenemos un amplio inventario de bombas, motores y partes  remanufacturadas, totalmente probadas y construidas en cumplimiento de la especificación OEM (Original Equipment Manufacturer).
+                Dado nuestro extenso inventario, podemos asegurar que tenemos las partes y refacciones que solicita y que están disponible para ser enviadas garantizando un tiempo de entrega adecuado.
             </p>
         </div>
 
@@ -34,7 +35,7 @@ if(!isset($_SESSION["username"])) {
                     <th>ID</th>
                     <th>Nombre</th>
                     <th>Imagen</th>
-                    <th>Descripcion</th>
+                    <th>Descripción</th>
                 </tr>
                 </thead>
 
@@ -43,7 +44,7 @@ if(!isset($_SESSION["username"])) {
                     <td>ID</td>
                     <td>Nombre</td>
                     <td>Imagen</td>
-                    <td>Descripcion</td>
+                    <td>Descripción</td>
                 </tr>
                 </tbody>
             </table>
