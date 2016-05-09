2341145f749ae6ea4604978c0171e3d87315838d
Cesar <cesars90_@hotmail.com>
2016-05-08 23:02:47 -0500

    Se agrego parametros a setFrom con variables en vez de strings

--- services/recoverService.php
+++ services/recoverService.php
@@ -12,10 +12,13 @@ $email = $_POST["email"];
 // get password from DB
 $result = getPasswordByEmail($email);
 $password = "";
+$emailFrom = "ecristerna.94@gmail.com";
+$nameFrom = "Hydrouministros Inteligentes";
 if ($result['message'] == 'OK') {
     $password = $result['password'];
 }  else {
-    die(json_encode($result));
+    header('HTTP/1.1 406 User not found');
+    die(json_encode(array('message' => 'ERROR', 'code' => 1337)));
 }
 
 require "../PHPMailer-master/PHPMailerAutoload.php";
@@ -25,14 +28,9 @@ $mail = new PHPMailer;
 //$mail->SMTPDebug = 3;                               // Enable verbose debug output
 
 $mail->IsSMTP(); //send via SMTP
-
 $mail->Host = "localhost";
-
-$mail->setFrom("ecristerna.94@gmail.com","Hydrouministros Inteligentes"); // info@hydrosumint.mx
-
-$mail->addAddress($mail);     // Add a recipient
-
-
+$mail->setFrom($emailFrom, $nameFrom); // info@hydrosumint.mx
+$mail->addAddress($email);     // Add a recipient
 $mail->isHTML(true);                                  // Set email format to HTML
 
 $mail->Subject = "Recupera tu passsword";
